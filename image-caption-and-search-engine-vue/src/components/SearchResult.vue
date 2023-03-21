692333ab248f186eb217011f9d56bf7a3fdb2e30
TheSky222 <1103465799@qq.com>
2023-03-21 16:34:32 +0800

    前端新增识图功能界面

--- image-caption-and-search-engine-vue/src/components/SearchResult.vue
+++ image-caption-and-search-engine-vue/src/components/SearchResult.vue
@@ -138,7 +138,7 @@
             id="picture"
             :class="{
               selectedOne: picture_text === 3,
-              picToPic: picTopic1 === false,
+              // picToPic: picTopic1 === true,
             }"
             @click="tranfer1('picToPic')"
           >
@@ -155,7 +155,7 @@
             <dl>
               <div v-if="recordsNum != 0">
                 <div
-                  v-for="item in imgAndCaption"
+                  v-for="item in imgAndCaption" :key="item"
                   align="left"
                   style="
                     display: flex;
@@ -205,7 +205,11 @@
                   justify-content: flex-start;
                 "
               >
-                <div v-for="item in imgAndCaption" align="left" class="P_item">
+                <div 
+                  v-for="item in imgAndCaption" :key="item"
+                  align="left" 
+                  class="P_item"
+                >
                   <div>
                     <img
                       style="height: 200px; border-radius: 10%"
@@ -243,24 +247,38 @@
 
           <el-col :span="11" v-if="picture_text == 3">
             <dl>
-              <div
-                style="
-                  display: flex;
-                  flex-wrap: wrap;
-                  justify-content: flex-start;
-                  min-width: 850px;
-                "
-              >
-                <div v-for="item in imgAndCaption1" align="left">
-                  <img
-                    style="
-                      height: 200px;
-                      margin-right: 15px;
-                      border-radius: 10%;
-                    "
-                    :src="item.url"
-                  />
-                </div>
+<!--              <div-->
+<!--                style="-->
+<!--                  display: flex;-->
+<!--                  flex-wrap: wrap;-->
+<!--                  justify-content: flex-start;-->
+<!--                  min-width: 850px;-->
+<!--                "-->
+<!--              >-->
+<!--                <div v-for="item in imgAndCaption1" align="left">-->
+<!--                  <img-->
+<!--                    style="-->
+<!--                      height: 200px;-->
+<!--                      margin-right: 15px;-->
+<!--                      border-radius: 10%;-->
+<!--                    "-->
+<!--                    :src="item.url"-->
+<!--                  />-->
+<!--                </div>-->
+<!--              </div>-->
+              <div>
+                <p>
+                  {{msg}}
+                </p>
+                <el-upload
+                    class="avatar-uploader"
+                    action="http://127.0.0.1:5000/clip"
+                    :show-file-list="false"
+                    :on-success="handleAvatarSuccess"
+                    :before-upload="beforeAvatarUpload">
+                  <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                  <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                </el-upload>
               </div>
             </dl>
           </el-col>
@@ -273,7 +291,7 @@
           <div id="allrelated">
             <div
               class="related"
-              v-for="rw in relatedWord"
+              v-for="rw in relatedWord" :key="rw"
               @click="searchRelated({ rw })"
             >
               {{ rw }}
@@ -357,7 +375,10 @@ export default {
       picture_text: 1,
       picTopic1: false,
       addToFavoritedialogVisible: false,
-      timer: ''
+      timer: '',
+      // 识图所需数据
+      msg: 'init',//识图结果
+      imageUrl: '',//上传图片地址
     };
   },
   created() {
@@ -462,33 +483,38 @@ export default {
         '<span style="color:red;">' + target + "</span>"
       );
     },
+    //识图界面
+    handleAvatarSuccess(res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw);
+      this.msg = res.output
+    },
     //以图搜图的展示
     //上传图片后的回调函数
-    handleAvatarSuccess(response) {
-      if (response[0] == "图片上传失败!") {
-        this.$message.error("图片上传失败!");
-      } else {
-        this.picTopic1 = true;
-        this.imgAndCaption1 = []; //<========================赋值到此
-        for (let i = 0; i < response.length; i++) {
-          this.imgAndCaption1.push({
-            url: response[i],
-          });
-        }
-      }
-    },
-    beforeAvatarUpload(file) {
-      const isJPG = file.type === "image/jpeg";
-      const isLt2M = file.size / 1024 / 1024 < 2;
-
-      if (!isJPG) {
-        this.$message.error("上传头像图片只能是 JPG 格式!");
-      }
-      if (!isLt2M) {
-        this.$message.error("上传头像图片大小不能超过 2MB!");
-      }
-      return isJPG && isLt2M;
-    },
+    // handleAvatarSuccess(response) {
+    //   if (response[0] == "图片上传失败!") {
+    //     this.$message.error("图片上传失败!");
+    //   } else {
+    //     this.picTopic1 = true;
+    //     this.imgAndCaption1 = []; //<========================赋值到此
+    //     for (let i = 0; i < response.length; i++) {
+    //       this.imgAndCaption1.push({
+    //         url: response[i],
+    //       });
+    //     }
+    //   }
+    // },
+    // beforeAvatarUpload(file) {
+    //   const isJPG = file.type === "image/jpeg";
+    //   const isLt2M = file.size / 1024 / 1024 < 2;
+    //
+    //   if (!isJPG) {
+    //     this.$message.error("上传头像图片只能是 JPG 格式!");
+    //   }
+    //   if (!isLt2M) {
+    //     this.$message.error("上传头像图片大小不能超过 2MB!");
+    //   }
+    //   return isJPG && isLt2M;
+    // },
     //文本栏，图片栏,搜图栏切换
     tranfer1(val) {
       if (val == "text") {
@@ -738,5 +764,29 @@ a {
   #gen {
     display: none;
   }
+}
+/* 上传界面样式 */
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409EFF;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
 }
 </style>
